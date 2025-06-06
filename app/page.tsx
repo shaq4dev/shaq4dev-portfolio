24e0e76023e15dd2fd3d36a268fbab96dd2400c6
Shaquille Blackwood <130801863+shaq4dev@users.noreply.github.com>
2025-06-05 20:27:52 -0400

    changed web to digital

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const Landing = () => {
                     <div className="flex flex-col items-center "> {/*mt-28 lg:mt-52*/}
                         <div className="text-center flex flex-col gap-3 lg:gap-5 w-[90%] lg:w-1/2">
                             <h1 className='font-black text-4xl md:text-6xl'>Front End Developer and UI/UX Designer</h1>
-                            <h3 className='font-semibold text-2xl md:text-4xl'>Crafting beautiful, responsive web experiences for modern brands.</h3>
+                            <h3 className='font-semibold text-2xl md:text-4xl'>Crafting beautiful, responsive digital experiences for modern brands.</h3>
                             <p className='text-md md:text-xl md:px-5 lg:px-0'>I design and build accessible, user-first web and mobile apps with performance and aesthetics in mind.</p>
                         </div>
                         <div className="flex flex-col gap-2 cta my-10 md:flex-row md:gap-5">
