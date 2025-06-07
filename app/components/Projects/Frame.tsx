54cea31b08e97332cff13e3aa423f26eb4b657bc
Shaquille Blackwood <130801863+shaq4dev@users.noreply.github.com>
2025-06-06 20:22:08 -0400

    images added within the template --- dummy image for now

--- app/components/Projects/Frame.tsx
+++ app/components/Projects/Frame.tsx
@@ -1,4 +1,13 @@
-export default function Frame({l, i}: {l:number, i: number}){
+'use client'
+
+import {useContext} from 'react'
+import {ThemeContext} from "@/app/context/ThemeContext";
+import Image from 'next/image'
+import Dummy from '@/public/dummy.jpg'
+
+export default function Frame({l, i, item}: {l:number, i: number, item: any}){
+
+    const theme = useContext(ThemeContext)
 
     return (
         <div className={
@@ -6,15 +15,18 @@ export default function Frame({l, i}: {l:number, i: number}){
                 l === 1 ? "h-[25rem]" :
                     l === 2 ? "h-96" :
                         l === 3 && i === 2 ? "h-80 col-span-2" :
-                            l === 4 && i === 0 ? "h-80 col-span-3" :
-                                l === 5 && i === 0  ? "h-[28rem] row-span-2 col-span-2" :
-                                    l === 6 && i === 0 ? "h-72" :
-                                        l === 7 && i === 0 || l === 7 && i === 6 ? "h-80 col-span-2" :
-                                            l === 8 && i === 0 || l === 8 && i === 3 || l === 8 && i === 4 || l === 8 && i === 7 ? "h-80 col-span-2" :
-                                                    // l === 8 && i === 3 ? "h-80 col-span-5" :
+                            l === 4 && i === 0 ? "h-64 col-span-3" :
+                                l === 5 && (i === 0 || i === 4)   ? "h-64 row-span-1 col-span-3" :
+                                    l === 6 && (i === 0 || i === 3 || i === 4) ? "h-64 col-span-2" :
+                                        l === 7 && i === 0 || l === 7 && i === 6 ? "h-72 col-span-2" :
+                                            l === 8 && i === 0 || l === 8 && i === 3 || l === 8 && i === 4 || l === 8 && i === 7 ? "h-64 col-span-2" :
                                     ""
             } 
-                    rounded-md bg-zinc-700`
-        }></div>
+            ${theme === 'light' ? 'bg-zinc-600/20' : 'bg-zinc-800'}
+                    rounded-md overflow-hidden`
+        }>
+                <Image src={Dummy} alt={item.label} className='object-cover w-full'/>
+            {/*<div>{item.label}</div>*/}
+        </div>
     )
 }
--- app/components/Projects/Stack.tsx
+++ app/components/Projects/Stack.tsx
@@ -32,7 +32,7 @@ export default function StackView(){
                         l === 2 ? "grid-cols-2" :
                             l === 3 ? "grid-cols-2 grid-rows-2" :
                                 l === 4 ? "grid-cols-3 grid-rows-2" :
-                                    l === 5 ? "grid-cols-4 grid-rows-2" :
+                                    l === 5 ? "grid-cols-3 grid-rows-2" :
                                         l === 6 ? "grid-cols-3 grid-rows-2" :
                                             l === 7 ? "grid-cols-3 grid-rows-3" :
                                                 l === 8 ? "grid-cols-3 grid-rows-4" :
@@ -42,7 +42,7 @@ export default function StackView(){
             }>
                 {
                     maxProject.length > 0 ?
-                        maxProject.map((item, index) => <Frame key={index} i={index} l={l} />) :
+                        maxProject.map((item, index) => <Frame key={index} i={index} l={l} item={item} />) :
                         <div>Projects Not Found</div>
                 }
             </div>
